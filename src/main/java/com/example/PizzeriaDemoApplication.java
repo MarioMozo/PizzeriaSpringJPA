669d71a827409942801f1bfa751a09b263eccd0e
MarioMozo <mario.psy@live.com>
2023-11-13 21:32:13 -0600

    Pizzeria con entidades creadas, CustomerEntity, OrderEntity, OrderItemEntity, PizzaEntity, OrderItemId

---
+++ src/main/java/com/example/PizzeriaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pizzeriademo/PizzeriaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzeriademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
